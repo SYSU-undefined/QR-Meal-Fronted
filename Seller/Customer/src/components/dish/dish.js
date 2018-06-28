7756c2589bdfc05f36a2feafdf1d89c103a2ac46
chenxu <chenxu2048@gmail.com>
2018-06-29 02:35:15 +0800

    完成菜品组件

--- Seller/Customer/src/components/dish/dish.js
+++ Seller/Customer/src/components/dish/dish.js
@@ -1,17 +1,29 @@
 import { Dish } from '@/models/dish';
 
-export default {
+/**
+ * @type {MpVueOption}
+ */
+const option = {
   props: {
     dish: Dish,
+    showTotal: Boolean,
   },
   methods: {
     add_dish() {
       this.dish.count += 1;
       this.$emit('add');
+      wx.dish = this.dish;
     },
     remove_dish() {
       this.dish.count -= 1;
       this.$emit('remove');
     },
   },
+  computed: {
+    totalPrice() {
+      return this.dish.count * this.dish.price;
+    },
+  },
 };
+
+export default option;
--- Seller/Customer/src/components/dish/index.vue
+++ Seller/Customer/src/components/dish/index.vue
@@ -2,42 +2,71 @@
   <div class="dish_panel">
     <img class="dish_image" :src="dish.image">
     <div class="dish_info">
-      <span>{{dish.name}}</span>
-      <span>{{dish.price}}</span>
-    </div>
-    <div class="button_panel">
-      <div v-if="dish.count != 0">
-        <i class="dish_btn fa fa-minus-circle" @click="remove_dish"/>
-        <span>{{dish.count}}</span>
+      <div class="dish_name">{{dish.name}}</div>
+      <div class="dish_price">&yen;{{showTotal ? dish.price : totalPrice}}</div>
+      <div class="button_panel">
+        <div v-if="dish.count != 0">
+          <i class="dish_btn fa fa-minus-circle" @click="remove_dish"/>
+          <div class="dish_count">{{dish.count > 99 ? '99+' : dish.count}}</div>
+        </div>
+        <i class="dish_btn fa fa-plus-circle" @click="add_dish"/>
       </div>
-      <i class="dish_btn fa fa-plus-circle" @click="add_dish"/>
     </div>
   </div>
 </template>
 
 
 <style scoped>
-
 .dish_image {
-  height: 100rpx;
-  width: 100rpx;
+  height: 200rpx;
+  width: 200rpx;
+  float: left;
+  margin-right: 20rpx;
 }
 
-.dish_panel>div, .button_panel>div {
+.button_panel div,
+.button_panel,
+.dish_price {
   display: inline-block;
 }
 
 .dish_info {
-  flex-grow: 1;
+  overflow: hidden;
+}
+
+.dish_count {
+  display: block;
+  width: 2em;
+  padding: 0 0.2em;
+  text-align: center;
+}
+
+.button_panel {
+  margin: 0 10rpx;
+  right: 15rpx;
+  position: absolute;
 }
 
 .dish_panel {
-  height: 100rpx;
   width: 100%;
-  display: flex;
-  padding: 20rpx;
+  padding: 15rpx;
   box-sizing: border-box;
   align-items: center;
+  position: relative;
+}
+
+img,
+.dish_info {
+  vertical-align: middle;
+}
+
+.dish_name {
+  width: 100%;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  font-size: 1.5em;
+  font-weight: bolder;
 }
 </style>
 
