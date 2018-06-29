afcb935d4ce64c83803db0ed02d86b92910ca7ea
chenxu <chenxu2048@gmail.com>
2018-06-30 04:46:35 +0800

    主页添加事件

---
+++ Seller/Customer/src/pages/index/vue.js
@@ -0,0 +1,38 @@
+/**
+ * @type {MpVueOption}
+ */
+const option = {
+  data() {
+    return {
+      userInfo: {},
+    };
+  },
+
+  methods: {
+    async getUserInfo() {
+      const { code } = await wx.loginAsync();
+      // TODO: login here
+      console.log(code);
+      ({ userInfo: this.userInfo } = await wx.getUserInfoAsync());
+    },
+    async scanCode() {
+      const { path } = await wx.scanCodeAsync();
+      if (path) {
+        wx.navigateTo({ url: path });
+      }
+    },
+    toOrders() {
+      wx.navigateTo({ url: '/pages/orders/main' });
+    },
+    toOrderStatus() {
+      wx.navigateTo({ url: '/pages/order/main' });
+    },
+  },
+
+  created() {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo();
+  },
+};
+
+export default option;
